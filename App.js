4a6ada58a609cd328d75ce24e7a1139b45eeb41d
unknown <jjj1305@hanmail.net>
2021-07-15 17:23:01 +0900

    lecture 6.3

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
+import { Ionicons } from '@expo/vector-icons';
 import { Text, Image } from 'react-native';
 
 const cacheImages = images => images.map(image =>{
@@ -12,6 +14,8 @@ const cacheImages = images => images.map(image =>{
   }
 });
 
+const cacheFonts = fonts => fonts.map(font => Font.loadAsync(font))
+
 export default function App() {
   const [isReady, setIsReady] = useState(false);
   const handleFinish = () => setIsReady(true);
@@ -20,7 +24,10 @@ export default function App() {
       require("./assets/loginBg.jpg"),
   "https://www.google.com/url?sa=i&url=https%3A%2F%2Febenezersuites.com%2Fhome%2Fairbnb-logo%2F&psig=AOvVaw1nvmxtozHJBe-U0j7iYZey&ust=1626415945104000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCND24de15PECFQAAAAAdAAAAABAI"
 ];
-console.log(cacheImages(images));
+  const fonts = [Ionicons.font]
+  const imagePromises = cacheImages(images);
+  const fontPromises = cacheFonts(fonts);
+  return Promise.all([...fontPromises, ...imagePromises])
   }
   return isReady ? (<Text>I'm ready</Text>) : (
   <AppLoading 
