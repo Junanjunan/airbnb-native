eb28eb9e2c1c3625e51881ec332d4e169f72a7de
unknown <jjj1305@hanmail.net>
2021-07-18 23:47:04 +0900

    lecture 8.6

--- screens/Auth/SignIn.js
+++ screens/Auth/SignIn.js
@@ -1,34 +1,45 @@
 import React, { useState } from "react";
-import {View, Text} from "react-native";
+import {StatusBar, KeyboardAvoidingView} from "react-native";
 import styled from "styled-components/native";
 import Btn from "../../components/Auth/Btn";
 import Input from "../../components/Auth/Input";
+import DismissKeyboard from "../../components/DismissKeyboard";
 
 const Container = styled.View`
     flex:1 ;
     justify-content: center;
+    align-items: center;
 `;
 
-const TextInput = styled.TextInput``;
+const InputContainer = styled.View`
+    margin-bottom: 30px;
+`;
 
 export default () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const handleSubmit = () => alert(`${username}${password}`);
     return(
-        <Container>
-            <Input 
-                value={username} 
-                placeholder="Username" 
-                autoCapitalize="none" 
-                stateFn={setUsername}
-            />
-            <Input 
-                value={password} 
-                placeholder="Password" 
-                stateFn={setPassword}    
-            />
-            <Btn text={"Sign In"} accent onPress={handleSubmit} />
-        </Container>
+        <DismissKeyboard>
+            <Container>
+                <StatusBar barStyle="light-content" />
+                <KeyboardAvoidingView behavior='position'>
+                <InputContainer>
+                    <Input 
+                        value={username} 
+                        placeholder="Username" 
+                        autoCapitalize="none" 
+                        stateFn={setUsername}
+                    />
+                    <Input 
+                        value={password} 
+                        placeholder="Password" 
+                        stateFn={setPassword}    
+                    />
+                </InputContainer>
+                <Btn text={"Sign In"} accent onPress={handleSubmit} />
+                </KeyboardAvoidingView>
+            </Container>
+        </DismissKeyboard>
     );
 };
