2567d3ac11faff373c5a24e95cba5b01d7c90fb7
unknown <jjj1305@hanmail.net>
2021-07-18 19:26:03 +0900

    lecture 8.3

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StatusBar } from "react-native";
 import styled from "styled-components/native";
 import { BlurView } from 'expo-blur';
+import Btn from "../components/Auth/Btn";
 
 const LOGO_URL = "https://blog.kakaocdn.net/dn/95jT7/btqQCAwEbDf/m30lmOx9xg7fKaPzQsFfJ1/img.jpg"
 
@@ -20,7 +21,11 @@ const Logo = styled.Image`
     height: 100px;
 `;
 
+const BtnContainer = styled.View``;
+
 export default ({ navigation }) => {
+    const goToSignUp = () => navigation.navigate("SignUp");
+    const goToSignIn = () => navigation.navigate("SignIn");
     return(
         <Container>
             <BlurView
@@ -33,6 +38,10 @@ export default ({ navigation }) => {
                     justifyContent: "center"
                 }}>
                 <Logo source={{uri:LOGO_URL}} />
+                <BtnContainer>
+                    <Btn onPress={goToSignUp} text={"Sign Up"} accent={true} />
+                    <Btn onPress={goToSignIn} text={"Sign In"} />
+                </BtnContainer>
             </BlurView>
             <Image source={require("../assets/loginBg.jpg")} />
             <StatusBar barStyle="light-content" />
