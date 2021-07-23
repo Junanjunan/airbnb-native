dec8790aa65cbd3392b35bb26d6762fa9da9b2b0
unknown <jjj1305@hanmail.net>
2021-07-23 22:27:02 +0900

    lecture 9.12

--- redux/roomsSlice.js
+++ redux/roomsSlice.js
@@ -12,20 +12,12 @@ const roomsSlice = createSlice({
     },
     reducers: {
         setExploreRooms(state, action){
-            const { explore } = state;
             const { payload } = action;
         if(payload.page === 1){
             state.explore.rooms = payload.rooms;
             state.explore.page = 1;
         } else{
-            payload.rooms.forEach(payloadRoom => {
-                const exists = explore.rooms.find(
-                    savedRoom => savedRoom.id === payloadRoom.id
-                    );
-                if(!exists){
-                    explore.rooms.push(payloadRoom);
-                }
-            });
+            state.explore.rooms = [...state.explore.rooms, ...payload.rooms];
         }
         },
         increasePage(state, action){
@@ -36,9 +28,9 @@ const roomsSlice = createSlice({
 
 export const { setExploreRooms, increasePage } = roomsSlice.actions;
 
-export const getRooms = page => async dispatch => {       // getRoos는 page argument와 함께 호출해야 함
+export const getRooms = page => async dispatch => {       
     try {
-        const {data: {results}} = await api.rooms(page);    // api.rooms도 page argument와 함께 호출해야 함
+        const {data: {results}} = await api.rooms(page);  
         dispatch(setExploreRooms({
             rooms: results,
             page,
